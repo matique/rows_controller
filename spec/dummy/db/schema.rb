6755af974040bca20fee4d741d264efa3f9467e9
matique <dittmar.krall@matique.de>
2013-03-01 09:30:32 +0100

    'bundle freezes; gem capybara?; Rails 3.2.12'

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(:version => 1) do
   create_table "orders", :force => true do |t|
     t.string   "name"
     t.string   "qty"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
