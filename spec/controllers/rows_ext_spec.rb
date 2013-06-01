b326af7d5b8d61b5f40c3e35f71ada16fa5dfc9b
matique <dittmar.krall@matique.de>
2013-06-01 16:10:32 +0200

    '...'

--- spec/controllers/rows_ext_spec.rb
+++ spec/controllers/rows_ext_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 describe RowsExtController do
   specify { should respond_to(:copy) }
-  specify { should respond_to(:multi_deletion) }
+#  specify { should respond_to(:multi_deletion) }
 end
