e39ab234bb5c6f93699ff451ba76151f828b6a0e
matique <dittmar.krall@matique.de>
2014-10-31 22:03:13 +0100

    'spec removed'

--- spec/controllers/rows_ext_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-describe RowsExtController do
-  specify { should respond_to(:copy) }
-#  specify { should respond_to(:multi_deletion) }
-end
--- spec/dummy/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Dummy::Application.routes.draw do
-  resources :orders do
-    get 'copy', :on => :member
-  end
-  resources :categories
-  root :to => 'orders#index'
-end
