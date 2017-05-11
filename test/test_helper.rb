35f8db8eb73a0996f62180f0e2862456cea0cf63
matique <dittmar.krall@matique.de>
2017-05-11 19:37:05 +0200

    '...'

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,9 +5,6 @@
 #end
 
 
-# Configure Rails Environment
-ENV["RAILS_ENV"] = "test"
-
 rails_version = `rails -v`.strip
 aster = '#' * 20
 puts "#{aster} Testing Ruby #{RUBY_VERSION}; #{rails_version} #{aster}"
@@ -18,11 +15,7 @@ dir = "dummy#{$1}"
 
 require File.expand_path("../#{dir}/config/environment.rb",  __FILE__)
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/#{dir}/db/migrate", __FILE__)]
-require "rails/test_help"
-
-# Filter out Minitest backtrace while allowing backtrace from other libraries
-# to be shown.
-#Minitest.backtrace_filter = Minitest::BacktraceFilter.new
+require 'rails/test_help'
 
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
