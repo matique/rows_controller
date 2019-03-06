080d022381fdb4b7dd01a5353c208ecf4d43de7b
matique <dittmar.krall@matique.de>
2019-03-06 16:52:18 +0100

    'v2.2.2'

--- lib/rows/version.rb
+++ lib/rows/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Rows
-#  VERSION = '2.2.2'     # 2019-05-06
-  VERSION = '2.2.1'     # 2019-02-18
+  VERSION = '2.2.2'     # 2019-05-06
+#  VERSION = '2.2.1'     # 2019-02-18
 #  VERSION = '2.2.0'
 end
