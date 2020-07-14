0d11cd609cb4e8482c87e89c30ce460ced9baff5
matique <dittmar.krall@matique.de>
2020-07-14 20:35:36 +0200

    'v3.0.3'

--- lib/rows/version.rb
+++ lib/rows/version.rb
@@ -1,7 +1,8 @@
 # rubocop: disable all
 
 module Rows
-  VERSION = '3.0.2'     # 2020-04-27
+  VERSION = '3.0.3'     # 2020-07-14
+# VERSION = '3.0.2'     # 2020-04-27
 # VERSION = '3.0.1'     # 2020-03-01
 # VERSION = '3.0.0'     # 2019-10-03
 # VERSION = '2.2.3'     # 2019-10-03
